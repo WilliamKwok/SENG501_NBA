f17dd102d9c5812a20c0c60897b53689385c603c
William Kwok <williamkwok92@gmail.com>
2015-04-11 12:55:01 -0600

    qplot on average height for teams vs wins

--- UPDATED_nba.r
+++ UPDATED_nba.r
@@ -60,3 +60,52 @@ Unique.nba.id.mapreduce.Val <-(from.dfs(Unique.nba.id.mapreduce))
 names(Unique.nba.id.mapreduce.Val)[1] <-"id"
 names(Unique.nba.id.mapreduce.Val)[2] <-"count"
 View(Unique.nba.id.mapreduce.Val)
+
+# TEST on Wins vs Height qplot from dataframe that contains multiple classifiers for team ONLY
+mapNBAStats <- function(.,lines)
+{
+  # key would be for the team only for now
+  key <- lines$teamname
+  #checkwin <- lines$win
+  #win <- ifelse(!is.null(checkwin),checkwin,0)
+  values <- cbind(lines$height, lines$weight, lines$minute, lines$fgm, lines$fga, lines$ftm, lines$fta, lines$tpm, lines$tpa, lines$orb, lines$drb, #11 
+                 lines$ast, lines$stl, lines$to, lines$blk, lines$pf, lines$pts, lines$teamscore, 1, lines$teamname, lines$win) # 12-20
+  keyval(key, values)
+  #keyval(unlist(subset(lines,select=c("id"))),1)
+}
+
+reduceNBAStats <- function(key, values)
+{ 
+  occurrences <- sum(values[,19])
+  avg_height <- format(round(mean(values[,1], na.rm=TRUE),2),nsmall=1)
+  avg_weight <- format(round(mean(values[,2], na.rm=TRUE),2),nsmall=0)
+  avg_minute <- format(round(mean(values[,3], na.rm=TRUE),2),nsmall=2)
+  avg_teamscore <- format(round(mean(values[,18], na.rm=TRUE),2),nsmall=2)
+  avg_pts <-format(round(mean(values[,17], na.rm=TRUE),2),nsmall=2)
+  wins <- sum(values[,21])
+  teamname <- key
+  #wins <- strtoi(wins,)
+  values <- cbind(wins, teamname, avg_teamscore, occurrences, avg_height, avg_weight, avg_minute, avg_pts)
+  
+  keyval(key, values)
+}
+
+NBAStats <- mapreduce(input ='/user/biadmin/nba3.csv', input.format =nba.in.format,
+                        map = mapNBAStats,
+                        reduce = reduceNBAStats
+                      )
+
+NBAStats <- as.data.frame(values(from.dfs(NBAStats)))
+NBAStats[order(NBAStats$wins),]
+View(NBAStats)
+
+#TRAINING Commented out for now 
+#data <- createDataPartition(NBAStats$avg_teamscore, p = 0.7, list = FALSE)
+#train <-NBAStats[data,] 
+#test  <-NBAStats[-data,] 
+
+#qplot(train$avg_height, train$avg_teamscore, color=train$teamname, xlab="height", ylab="avg_teamscore")
+
+#NBAStats$wins NEEDS to be fixed for display
+qplot(NBAStats$avg_height, NBAStats$wins, color=NBAStats$teamname, xlab="height", ylab="wins")
+
